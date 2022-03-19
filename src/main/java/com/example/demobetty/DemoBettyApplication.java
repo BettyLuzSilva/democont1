81642f0aa3ab3bffe40cacdc8d67ac522dab8a24
Betty Luz <bsilva@bcp.com.pe>
2022-03-19 18:33:34 -0500

    carga inicial

---
+++ src/main/java/com/example/demobetty/DemoBettyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBettyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobetty/DemoBettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
